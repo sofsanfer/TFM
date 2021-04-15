183174e9e8305e4c2e5a76b68ba260810226e9cf
sofsanfer <sofiasntg@gmail.com>
2021-04-15 10:45:33 +0200

    Terminada cl_max'_detallada

--- Consistencia.thy
+++ Consistencia.thy
@@ -3290,11 +3290,6 @@ proof (rule ccontr)
   with 1 show False ..
 qed
 
-lemma 
-  assumes "insert a A = A"
-  shows "a \<in> A"
-  oops
-
 lemma cl_max'_detallada:
   assumes "pcp C"
   assumes "subset_closed C"
@@ -3308,18 +3303,34 @@ proof -
       by (rule subset_insertI) 
     have "pcp_lim C S = insert F (pcp_lim C S)"
       using assms(1) assms(2) \<open>insert F (pcp_lim C S) \<in> C\<close> \<open>pcp_lim C S \<subseteq> insert F (pcp_lim C S)\<close> by (rule cl_max)
+    then have "insert F (pcp_lim C S) \<subseteq> pcp_lim C S"
+      by (rule equalityD2)
+    then have "F \<in> pcp_lim C S \<and> pcp_lim C S \<subseteq> pcp_lim C S"
+      by (simp only: insert_subset)
     thus "F \<in> pcp_lim C S"
-      by blast (*Pendiente*)
+      by (rule conjunct1)
   qed
   show "insert F (insert G (pcp_lim C S)) \<in> C \<Longrightarrow> F \<in> pcp_lim C S \<and> G \<in> pcp_lim C S"
-  proof -
+  proof (rule conjI)
     assume "insert F (insert G (pcp_lim C S)) \<in> C"
-    have "pcp_lim C S \<subseteq> insert F (insert G (pcp_lim C S))"
-      by blast (*Pendiente*)
+    have "pcp_lim C S \<subseteq> insert G (pcp_lim C S)"
+      by (rule subset_insertI)
+    then have "pcp_lim C S \<subseteq> insert F (insert G (pcp_lim C S))"
+      by (rule subset_insertI2)
     have "pcp_lim C S = insert F (insert G (pcp_lim C S))"
       using assms(1) assms(2) \<open>insert F (insert G (pcp_lim C S)) \<in> C\<close> \<open>pcp_lim C S \<subseteq> insert F (insert G (pcp_lim C S))\<close> by (rule cl_max)
-    thus "F \<in> pcp_lim C S \<and> G \<in> pcp_lim C S"
-      by blast (*Pendiente*)
+    then have "insert F (insert G (pcp_lim C S)) \<subseteq> pcp_lim C S"
+      by (rule equalityD2)
+    then have 1:"F \<in> pcp_lim C S \<and> (insert G (pcp_lim C S)) \<subseteq> pcp_lim C S"
+      by (simp only: insert_subset)
+    thus "F \<in> pcp_lim C S"
+      by (rule conjunct1)
+    have "insert G (pcp_lim C S) \<subseteq> pcp_lim C S"
+      using 1 by (rule conjunct2)
+    then have "G \<in> pcp_lim C S \<and> pcp_lim C S \<subseteq> pcp_lim C S"
+      by (simp only: insert_subset)
+    thus "G \<in> pcp_lim C S"
+      by (rule conjunct1)
   qed
 qed
 
